213484bfbe65960e652d2880c33353f07eac6e22
Fahim-Hossain-0 <fahimhossain735@gmail.com>
2023-11-07 03:09:15 +0600

    first commit

---
+++ text.js
@@ -0,0 +1,19 @@
+var pranto = 79;
+var tom = 66;
+var jane = 95;
+var peter = 56;
+var john = 40;
+
+var grade = [80, 60, 50, 40, 39];
+
+if (pranto >= grade[0]) {
+    console.log("A");
+} else if (pranto >= grade[1]) {
+    console.log("B");
+} else if (pranto >= grade[2]) {
+    console.log("C");
+} else if (pranto >= grade[3]) {
+    console.log("D");
+} else {
+    console.log("F");
+}
